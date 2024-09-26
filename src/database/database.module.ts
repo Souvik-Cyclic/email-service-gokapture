5f5417f58dca82e75eeb48d877db6df3db088ea4
Souvik Kumar <souvik.cyc@gmail.com>
2024-09-26 19:38:55 +0530

    .

--- src/database/database.module.ts
+++ src/database/database.module.ts
@@ -7,11 +7,12 @@ import { EmailTracking } from '../email/schema/email.entity';
   imports: [
     TypeOrmModule.forRoot({
       type: 'postgres',
-      host: process.env.DB_HOST,
-      port: +process.env.DB_PORT,
-      username: process.env.DB_USERNAME,
-      password: process.env.DB_PASSWORD,
-      database: process.env.DB_DATABASE,
+    //   host: process.env.DB_HOST,
+    //   port: +process.env.DB_PORT,
+    //   username: process.env.DB_USERNAME,
+    //   password: process.env.DB_PASSWORD,
+    //   database: process.env.DB_DATABASE,
+      url: process.env.DATABASE_URL,
       entities: [EmailTracking],
       synchronize: true, // to disable in prod
     }),
