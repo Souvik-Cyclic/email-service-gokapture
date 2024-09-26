c64cbe9f24c7a5c3023ba3f354bfbf35a483faf3
Souvik Kumar <souvik.cyc@gmail.com>
2024-09-26 19:52:24 +0530

    .

--- src/main.ts
+++ src/main.ts
@@ -4,7 +4,11 @@ import * as bodyParser from 'body-parser';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  // app.use(bodyParser.text({ limit: '50mb' }));
+  app.use(bodyParser.json({ limit: '50mb' })); // For JSON payloads
+  app.use(bodyParser.raw({ limit: '50mb' }));
   app.use(bodyParser.text({ limit: '50mb' }));
+  app.use(bodyParser.urlencoded({ limit: '50mb', extended: true })); // For URL-encoded data
   await app.listen(3000);
 
 }
